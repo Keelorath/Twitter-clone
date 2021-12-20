05409e4b4bc2f3d6db1af91fdcd6f7d5706b0cfc
David <magyda991@gmail.com>
2021-12-20 15:18:05 +0100

    Prepare Widgets

---
+++ src/Widgets.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Widgets() {
+    return (
+        <div className='widgets'>
+            <h2>Widget placeholder</h2>
+        </div>
+    )
+}
+
+export default Widgets
