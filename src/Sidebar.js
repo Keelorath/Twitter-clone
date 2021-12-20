aad2fdc9e816850485d31c2a2041e70e9875e6bc
David <magyda991@gmail.com>
2021-12-20 12:53:52 +0100

    Start sidebar component

---
+++ src/Sidebar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import '.Sidebar.css';
+
+function Sidebar(){
+    return(
+        <div className="sidebar">
+            {/* Icon*/ }
+            {/* SidebarOptions*/ }
+            {/* SidebarOptions*/ }
+            {/* SidebarOptions*/ }
+            {/* SidebarOptions*/ }
+            {/* SidebarOptions*/ }
+            {/* SidebarOptions*/ }
+
+            {/* tweet button*/ }
+        </div>
+
+    )
+}
+
+export default Sidebar;
