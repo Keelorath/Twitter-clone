3e2661eaecc4fa53dbd2cd0da35175dd06cbf706
David <magyda991@gmail.com>
2021-12-20 13:48:59 +0100

    Display sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import './App.css';
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <div className="App">
       <h1>Twitter-clone</h1>
       {/*Sidebar */}
+      <Sidebar />
       {/*Feed */}
       {/*Widgets */}
     </div>
