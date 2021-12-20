f13d5631fd10d3ab3795a3c4f5aac3f7ec7a00af
David <magyda991@gmail.com>
2021-12-20 13:48:11 +0100

    Import icons

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,29 @@
 import React from "react";
-import '.Sidebar.css';
+import './Sidebar.css';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import SidebarOption from "./SidebarOption";
+import HomeIcon from '@mui/icons-material/Home';
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import MessageIcon from '@mui/icons-material/Message';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import ListIcon from '@mui/icons-material/List';
+import PersonIcon from '@mui/icons-material/Person';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 
 function Sidebar(){
     return(
         <div className="sidebar">
             {/* Icon*/ }
+            <TwitterIcon />
+            <SidebarOption Icon={HomeIcon} text="Home"/>
+            <SidebarOption Icon={SearchIcon} text="Explore"/>
+            <SidebarOption Icon={NotificationsIcon} text="Notifications"/>
+            <SidebarOption Icon={MessageIcon} text="Messages"/>
+            <SidebarOption Icon={BookmarkIcon} text="Bookmarks"/>
+            <SidebarOption Icon={ListIcon} text="Lists"/>
+            <SidebarOption Icon={PersonIcon} text="Profile"/>
+            <SidebarOption Icon={MoreHorizIcon} text="More"/>
             {/* SidebarOptions*/ }
             {/* SidebarOptions*/ }
             {/* SidebarOptions*/ }
